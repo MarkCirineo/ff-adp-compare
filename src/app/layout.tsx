636d14273b76c013525d25f29ca6d7d14e9d4fd8
markcirineo <markcirineo22@gmail.com>
2026-05-21 20:13:15 -0400

    fix: suppress hydration warnings

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <div className="app-layout">
           {children}
